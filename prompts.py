6fc20ab04bda6fb95cf654ef234ded96297c753e
bala1802 <bala1802@live.com>
2024-05-01 14:12:58 +0530

    initialized alpaca prompt

---
+++ prompts.py
@@ -0,0 +1,14 @@
+'''
+1. Alpaca Prompt
+'''
+
+ALPACA_PROMPT = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
