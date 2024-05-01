bb0662e0fde0216058141d06ee744d7ed459649c
bala1802 <bala1802@live.com>
2024-05-01 15:16:23 +0530

    fixed model

--- model.py
+++ model.py
@@ -2,11 +2,10 @@ from unsloth import FastLanguageModel
 import config
 
 def initialize_model_tokenizer():
-    model, tokenizer = FastLanguageModel.from_pretrained(
-                                    model_name = config.LLAMA_3_8B
-                                    max_seq_length = config.MAX_SEQ_LENGTH,
-                                    dtype = config.d_TYPE,
-                                    load_in_4bit = config.LOAD_IN_4BIT)
+    model, tokenizer = FastLanguageModel.from_pretrained(model_name = config.LLAMA_3_8B,
+                                                         max_seq_length = config.MAX_SEQ_LENGTH,
+                                                         dtype = config.d_TYPE,
+                                                         load_in_4bit = config.LOAD_IN_4BIT)
     return model, tokenizer
 
 if __name__ == "__main__":
